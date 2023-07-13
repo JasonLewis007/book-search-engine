6a86f00976e26dbbc1a7a28c602b3dff1afa38b6
Jason Lewis <pensfan5050@gmail.com>
2023-07-13 18:04:11 -0400

    added code

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloProvider } from "@apollo/client";
+import ApolloClient from "apollo-boost";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
